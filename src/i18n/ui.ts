bc25144c216bcc82d0fe8a73c8c7498fa699619b
enbraining <me@xolving.com>
2024-01-06 09:01:10 +0900

    pioosrdf

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -17,16 +17,9 @@ export const ui = {
     'index.name': '김동학',
     'index.desc':
       `<p><b>숨겨진</b> 요소를 찾는 것이 재미있는 개발자 김동학입니다.</p>` +
-      `<p>남들과 소통하며 얻는 <b>새로운</b> 지식을 경험하고, 공유하며 성장합니다.</p>` +
-      `<p></br>범죄심리학이나, 화약학에 관심이 있습니다.</p>`,
-    'index.currentWork': `      <p>
-      현재
-      <b>
-        <a class="external-link" href="https://github.com/GSM-MSG/" target="_blank"
-          >MSG</a
-        >
-      </b>에서
-      개발하고 있습니다.
-    </p>`,
+      `<p>남들과 소통하며 얻는 <b>새로운</b> 지식을 경험하고, 공유하며 성장합니다.</p>`,
+    'index.currentWork':
+      `<p></br>범죄심리학이나, 화약학에 관심이 있습니다.</p>` +
+      `<p>현재<b><a class="external-link" href="https://github.com/GSM-MSG/" target="_blank">MSG</a></b>에서 개발하고 있습니다.</p>`,
   },
 } satisfies Record<Language, { [key: string]: string }>;
