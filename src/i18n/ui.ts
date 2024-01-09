93a0711b215b664fe04f51c33086a7ee8f56a975
jgh진건희 <132252115+GSMIOTjgh@users.noreply.github.com>
2024-01-09 19:38:36 +0900

    Update ui.ts

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -19,7 +19,7 @@ export const ui = {
       `<p>항상 사람과 <b>교류</b>하고 <b>성장</b>에 기쁨을 느끼는 개발자 진건희입니다.</p>` +
       `<p>남들과 소통하며 얻는 <b>새로운</b> 지식을 경험하고, 공유하며 성장합니다.</p>`,
     'index.currentWork':
-      `<p>심리학이나, 악기에 관심이 많으며,</p>` +
-      `<p>현재 <b><a class="external-link" href="https://github.com/GSM-MSG/" target="_blank">MSG</a></b>에서 개발하고 있습니다.</p>`,
+      `<p>프론트 엔드 개발자이며 디자인에도 관심을 두고 있습니다,</p>` +
+      `<p>현재 <b><a class="external-link" href="https://github.com/kody-gsm" target="_blank">Kody</a></b>에 소속되어 있습니다.</p>`,
   },
 } satisfies Record<Language, { [key: string]: string }>;
